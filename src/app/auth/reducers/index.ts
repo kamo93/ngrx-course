ff74135ec0f227ec91265edfda91dbdc817f78dd
Kevin Mancera <kevin_andres_@hotmail.com>
2019-10-04 21:57:26 -0500

    todo seccion 2

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,28 @@
+import { User } from '../model/user.model';
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer,
+  createReducer,
+  on
+} from '@ngrx/store';
+import * as AuthActions from '../action-types';
+
+export interface AuthState {
+  user: User;
+}
+
+export const initialAuthState: AuthState = {
+  user: undefined
+};
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    };
+  })
+);
